78da46d344786b013729d058f529affa90a3f08a
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-03 18:08:47 -0500

    add cars-data

---
+++ data/car-data.js
@@ -0,0 +1,9 @@
+const cars = [
+  {model: 'Corvette', fast: true, _id: 125223},
+  {model: 'Mustang', fast: false, _id: 127904},
+  {model: 'charger', fast: false, _id: 139608},
+]
+
+export { 
+	cars
+}
